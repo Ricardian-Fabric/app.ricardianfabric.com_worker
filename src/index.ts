6533121da7726f017df6c66246478dc5a57d131a
StrawberryChocolateFudge <hopeter@protonmail.com>
2022-08-31 18:12:42 +0100

    added NotFound page and CORS

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,83 @@ export enum Paths {
   dep,
   contract,
 }
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+};
+
+const notFoundHTML = (link: string) => `
+<html>
+  <head>
+    <title>Not Found</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        body {
+      background: radial-gradient(lightgrey 3px, transparent 4px),
+        radial-gradient(lightgrey 3px, transparent 4px),
+        linear-gradient(#fff 4px, transparent 0),
+        linear-gradient(
+          45deg,
+          transparent 74px,
+          transparent 75px,
+          #a4a4a4 75px,
+          #a4a4a4 76px,
+          transparent 77px,
+          transparent 109px
+        ),
+        linear-gradient(
+          -45deg,
+          transparent 75px,
+          transparent 76px,
+          #a4a4a4 76px,
+          #a4a4a4 77px,
+          transparent 78px,
+          transparent 109px
+        ),
+        #fff;
+      background-size: 109px 109px, 109px 109px, 100% 6px, 109px 109px,
+        109px 109px;
+      background-position: 54px 55px, 0px 0px, 0px 0px, 0px 0px, 0px 0px;
+      font-family: Sans-Serif;
+    }
+
+    a{
+      cursor: pointer;
+      background-color: black;
+      border-radius: 20px;
+      padding: 5px;
+      color: white !important;
+      border: none;
+      width: 200px;
+      margin: 0 auto;
+      text-decoration: none;
+    
+    }
+        .box{
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          height: 60%;
+        }
+        .container{
+          margin: 0 auto;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          text-align: center;
+        }
+      </style>
+  <body>
+    <div class="box"> 
+      <div class="container">
+   	  <h2>Not Found</h2>
+      <p>If you just created a Ricardian Contract, you need to wait for the transaction to be mined before it shows up here.</p>
+      <a href="${link}">See it on Arweave.net</a>
+      </div>
+</div>
+  </html>
+</html>`;
 
 async function fetchGraphql(txId: string) {
   const results = await fetch("https://arweave.net/graphql", {
@@ -41,6 +118,7 @@ async function fetchRouter(path: Paths, urlToFetch: string, arweaveTx: string) {
   const init = {
     headers: {
       "content-type": getContentType(path),
+      ...corsHeaders,
     },
   };
 
@@ -48,7 +126,7 @@ async function fetchRouter(path: Paths, urlToFetch: string, arweaveTx: string) {
     const data = await fetchGraphql(arweaveTx);
 
     if (data.transaction === null) {
-      return notFound(init);
+      return notFound(init, arweaveTx);
     }
 
     const acceptableContract = isAcceptableContract(data.transaction);
@@ -56,18 +134,18 @@ async function fetchRouter(path: Paths, urlToFetch: string, arweaveTx: string) {
     if (acceptableContract) {
       return await fetchText(urlToFetch, init);
     } else {
-      return notFound(init);
+      return notFound(init, arweaveTx);
     }
   } else {
     return await fetchText(urlToFetch, init);
   }
 }
 
-async function notFound(init: any): Promise<Response> {
-  return new Response(
-    "Not Found, If You just uploaded a contract wait a few minutes for the transaction to be mined!",
-    { ...init, status: 404 }
-  );
+async function notFound(init: any, txId: string): Promise<Response> {
+  return new Response(notFoundHTML(`https://arweave.net/${txId}`), {
+    ...init,
+    status: 404,
+  });
 }
 
 async function fetchText(urlToFetch: string, init: any): Promise<Response> {
@@ -151,8 +229,14 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const requestUrl = new URL(request.url);
-    const [currentPath, link, arweaveTx] = await parsePaths(requestUrl, env);
-    return fetchRouter(currentPath, link, arweaveTx);
+    //Handle cors prefligh request
+
+    if (request.method === `OPTIONS`) {
+      return new Response(null, { headers: { ...corsHeaders } });
+    } else {
+      const requestUrl = new URL(request.url);
+      const [currentPath, link, arweaveTx] = await parsePaths(requestUrl, env);
+      return fetchRouter(currentPath, link, arweaveTx);
+    }
   },
 };
